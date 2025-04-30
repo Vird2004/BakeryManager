7046ef26d483255011a6661e3df8882ff1559e98
grangerlangdon <reddragon.vird04@gmail.com>
2025-04-30 23:33:32 +0700

    #Add DbContext

--- BakeryManager/Models/ProductModel.cs
+++ BakeryManager/Models/ProductModel.cs
@@ -1,6 +1,25 @@
-﻿namespace BakeryManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BakeryManager.Models
 {
     public class ProductModel
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Yêu cầu nhập tên sản phẩm.")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Yêu cầu nhập mô tả sản phẩm.")]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0.")]
+        public decimal Price { get; set; }
+        public string Slug { get; set; }
+
+        public string Image { get; set; }
+        public string Status { get; set; }
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
     }
 }
