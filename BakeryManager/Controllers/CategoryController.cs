d45149a6b97d9d89c3ef708f3f3f9cbbc5aaa62a
grangerlangdon <reddragon.vird04@gmail.com>
2025-05-01 03:21:25 +0700

    Product By Category

--- BakeryManager/Controllers/CategoryController.cs
+++ BakeryManager/Controllers/CategoryController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BakeryManager.Models;
+using BakeryManager.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BakeryManager.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly DataContext _dataContext;
+
+        public CategoryController(DataContext dataContext)
         {
-            return View();
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index(string Slug = "")
+        {
+            CategoryModel category = _dataContext.Categories.Where(c => c.Slug == Slug).FirstOrDefault();
+            
+            if(category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+            return View(await productsByCategory.OrderByDescending(c=>c.Id).ToListAsync());
         }
     }
 }
