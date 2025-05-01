042cbe4956513faf6625000d11ef00dfb2ebb640
grangerlangdon <reddragon.vird04@gmail.com>
2025-05-01 20:46:30 +0700

    Cart Index

--- BakeryManager/Program.cs
+++ BakeryManager/Program.cs
@@ -11,7 +11,16 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,7 +29,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//session
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
