ca68ae3a753ed8c5eb8ab62fb18801c94c2348c0
grangerlangdon <reddragon.vird04@gmail.com>
2025-05-08 10:18:41 +0700

    Create User

--- BakeryManager/Controllers/AccountController.cs
+++ BakeryManager/Controllers/AccountController.cs
@@ -19,10 +19,37 @@ namespace BakeryManager.Controllers
         {
             return View();
         }
+         
 
         public async Task<IActionResult> Login()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUserModel newUser = new AppUserModel { UserName = user.UserName, Email = user.Email };
+                IdentityResult result = await _userManager.CreateAsync(newUser, user.Password);
+                if (result.Succeeded)
+                {
+                    TempData["success"] = "Tạo thành viên thành công";
+                    return Redirect("/account");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
         }
     }
 }
