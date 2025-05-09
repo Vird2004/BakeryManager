384e07783e593a422b7e3a356471fc11a1258795
grangerlangdon <reddragon.vird04@gmail.com>
2025-05-09 21:21:35 +0700

    Order View

--- BakeryManager/Models/OrderDetails.cs
+++ BakeryManager/Models/OrderDetails.cs
@@ -8,5 +8,7 @@
         public long ProductId { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+
+        public ProductModel Product { get; set; }
     }
 }
